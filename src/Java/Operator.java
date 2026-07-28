61553631af2d33439db0fe474a9e5ca4854d8d04
Shrawni Phondke <shrawniphondke09@example.com>
2026-07-28 21:11:24 +0530

    update operator

--- src/Java/Operator.java
+++ src/Java/Operator.java
@@ -1,30 +1,74 @@
 package Java;
 
 public class Operator {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
-    int a = 10;
-    int b = 20;
-    int sum = a + b;
-    int sub = a - b;
-    int mul = a*b;
-    int div = a / b;
-    int mol = a % b ;
+        //Operand
+        //Operator
+        //Operand
+
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mol = a % b;
 //      System.out.println(sum);
 //      System.out.println(sub);
 //      System.out.println(mul);
 //      System.out.println(div);
 //      System.out.println(mol);
 
+//        BODMAS
+        int result = (10 + 5) * 4 / 2 % 7 - 3;
+//        System.out.println(result);
 
-    int result = (10 + 5) * 4 / 2 % 7 - 3;
-        System.out.println(result);
+        int x = 5;
 
-    int x = 5;
+        x = x++ + ++x;
 
-        x = x++ + x++;
+//        System.out.println(x);
 
-        System.out.println(x);
-        System.out.println(x);
-}
+        int j = 44;
+        j = 2;
+//        System.out.println(j);
+
+        int k = 23;
+        int l = 22;
+
+//        System.out.println(k == l);
+//        System.out.println(k != l);
+//        System.out.println(k < l);
+//        System.out.println(k > l);
+//        System.out.println(k <= l);
+//        System.out.println(k >= l);
+
+        int age = 18;
+//
+//        if (age > 18 && age < 30) {
+//
+//            System.out.println("eligible for voting");
+//        } else {
+//            System.out.println("not eligible for voting");
+//        }
+
+       int score = 90;
+        if(score  > 70 || score < 85){
+            System.out.println("Admission");
+        }
+        else{
+            System.out.println("not Admission");
+        }
+
+        boolean isLoggedIn = true;
+
+        if (!isLoggedIn) {
+            System.out.println("Please login first.");
+        } else {
+            System.out.println("Welcome!");
+        }
+
+    }
 }
