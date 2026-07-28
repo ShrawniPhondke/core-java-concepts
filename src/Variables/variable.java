36d0799e1dc27ce37f90374150e7e863eeaa7fb3
Shrawni Phondke <shrawniphondke09@example.com>
2026-07-28 17:16:38 +0530

    Initial commit

---
+++ src/Variables/variable.java
@@ -0,0 +1,69 @@
+package Variables;
+
+public class variable {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        byte b = 20;
+//        char c = 'a';
+//        double d = 2.5;
+//        boolean e = true;
+//        float f = 56.0f;
+//        short h = 234;
+//        String s = "SHRAWNI";
+//       System.out.println("int =" +a+
+//               ", byte = " +b+
+//               ",char = " +c+
+//               ",double = " +d+
+//               ",boolean = "+e+
+//               ",float = " +f+
+//               ",short = " +h+
+//               ",string = " +s );
+
+//            int x = 10;
+//            int y ;
+//            y = x;
+//
+//            System.out.println(y);
+
+
+            int x = 5;
+            int y = 10;
+            int z ;
+            z = x; // z= 5
+            x = y ; // x = 10
+            y = z;
+
+//           System.out.println(x);
+//           System.out.println(y);
+
+
+           int a = 8;
+           int b = 12;
+           b = a;
+           a = 20;
+
+//         System.out.println(a);
+//         System.out.println(b);
+
+           int  m = 5;  //a
+           int  n = 10;  // b
+           int  q = 15;   // c
+           m = n;
+           n = q;
+           q = 50;
+//           System.out.println(m);
+//           System.out.println(n);
+//           System.out.println(q);
+
+          int p = 10;
+          int o = 20;
+          o = p;
+          p = 30;
+          o = p;
+          System.out.println(p);
+          System.out.println(o);
+
+
+    }
+}
