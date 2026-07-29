8bcd03da57d7b6c44e3717317ab79399694d358e
Shrawni Phondke <shrawniphondke09@example.com>
2026-07-29 19:51:40 +0530

    Practice new variable

--- src/Java/variable.java
+++ src/Java/variable.java
@@ -38,10 +38,10 @@ public class variable {
 //           System.out.println(y);
 
 
-           int a = 8;
-           int b = 12;
-           b = a;
-           a = 20;
+//           int a = 8;
+//           int b = 12;
+//           b = a;
+//           a = 20;
 
 //         System.out.println(a);
 //         System.out.println(b);
@@ -63,6 +63,103 @@ public class variable {
           o = p;
           System.out.println(p);
           System.out.println(o);
+
+          int h = 1;
+          int g = 3;
+          int temp =h;
+          h = (int)g;
+          g = temp;
+
+       // System.out.println(h);
+        //System.out.println(g);
+
+          int s = 25;
+//        int r = 36;
+//        s = s + r ;
+//        r = s - r;
+//        s = s - r;
+//      System.out.println(s);
+//      System.out.println(r);
+
+//        int a = 10;
+//        int b = 20;
+//        int c = a; // c = 10
+//
+//        a = b;  // a= 20
+//        b = c;  // b= 10
+//
+//        c = a + b; // 20 + 10 == 30
+//        a = c - a; // 30 - 20 == 10
+//        b = c - b; // 30 - 10 == 20
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+
+        int a = 8;
+        int b = 12;
+
+        a = a + b;  //20
+        b = a - b;  //8 // 9 // 8
+        a = a - b;  //12   //13 // 12
+
+        int c = a++ + ++b + --a + b--;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // 12 + 9 + 12 + 9
+
+        int a = 5; // 6 // 7
+//        int b = a; // 5 // 6 // 5
+//        int c = ++a + b++ + a++ + --b;
+////     6 + 5 + 6 + 5
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+
+//        int a = 3; //4 // 3
+//        int b = 4; // 5 // 6
+//
+//        int c = a++ + ++b + --a + b++;
+////        3 + 5 + 3 + 5
+//        System.out.println(a); //3
+//        System.out.println(b);// 6
+//        System.out.println(c); //16
+
+//        int x = 2;   // 3 // 4 // 5
+//        int y = 5;   // 6 // 5
+//
+//        int z = ++x + y++ + x++ + --y + ++x;
+//       // 3 + 5 + 3 + 5 + 5
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+
+
+//        int a = 10; // 11 // 12 // 11
+//        int b = 20; // 19 // 20 // 21
+//
+//        int c = a++ + ++a + --b + b++ + a-- + ++b;
+//        // 10 +   12  + 19 +  19 +  12 + 21
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+
+//        int x = 10;
+//        int y = x; //10
+//        x = y; // 10
+//        y = x++;
+//        x = ++y;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
 
 
     }
