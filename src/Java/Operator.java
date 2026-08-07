a989cc893c81d6d547ff560ce8e1470ad3309b78
Shrawni Phondke <shrawniphondke09@example.com>
2026-08-07 12:59:29 +0530

    Operator function

--- src/Java/Operator.java
+++ src/Java/Operator.java
@@ -8,13 +8,13 @@ public class Operator {
         //Operand
 
 
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
-        int sub = a - b;
-        int mul = a * b;
-        int div = a / b;
-        int mol = a % b;
+//        int a = 10;
+//        int b = 20;
+//        int sum = a + b;
+//        int sub = a - b;
+//        int mul = a * b;
+//        int div = a / b;
+//        int mol = a % b;
 //      System.out.println(sum);
 //      System.out.println(sub);
 //      System.out.println(mul);
@@ -40,13 +40,26 @@ public class Operator {
         int k = 23;
         int l = 22;
 
-        System.out.println(k == l);
-        System.out.println(k != l);
-        System.out.println(k < l);
-        System.out.println(k > l);
-        System.out.println(k <= l);
-        System.out.println(k >= l);
+//        System.out.println(k == l);
+//        System.out.println(k != l);
+//        System.out.println(k < l);
+//        System.out.println(k > l);
+//        System.out.println(k <= l);
+//        System.out.println(k >= l);
 
+
+
+        int a = 10;
+        int b = 20;
+        int c;
+        c = a; // c= 10
+        a = b;  // a = 20
+        b= 50;  // b = 50
+        b = c;  // b = 10
+        a= 100;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
 
     }
