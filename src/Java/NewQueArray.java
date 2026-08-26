0eb2dd1dd1f996346aea8f02950d7ecb6244cb6f
Shrawni Phondke <shrawniphondke09@example.com>
2026-08-26 16:20:11 +0530

    Arrat

---
+++ src/Java/NewQueArray.java
@@ -0,0 +1,51 @@
+package Java;
+import java.util.Arrays;
+public class NewQue {
+
+
+    public static void main(String []args){
+
+//        int a[] = {10 , 20 , 30 , 40 , 50 , 60 , 70 , 80};
+
+//        System.out.println(Arrays.toString(a));
+//        System.out.println(a[2]);
+//        a[3] = 90;
+//        System.out.println(Arrays.toString(a));
+//        System.out.println(a.length);
+//
+//        for(int i = a.length - 1 ; i >= 0 ; --i){
+//            System.out.println(a[i]);
+//        }
+
+
+//        String s[] = {"h","e","l","l","o"};
+//
+//        for(int i= s.length-1 ; i >= 0 ; i--){
+//            System.out.println(s[i]);
+//        }
+
+
+//        String str = "java";
+//        for(int i = str.length() - 1 ; i >= 0 ; i-- ){
+//            System.out.print(str.charAt(i));
+//        }
+//
+//        String SS = "hiii"
+//        for (int j = 0 ; j < SS.length(); j++){
+//            System.out.print(SS.charAt(j) + " ");
+//        }
+
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for(int i = 0 ; i < numbers.length ; i++){
+//            System.out.print(numbers[i] + " ");
+        }
+
+        for(int i = numbers.length - 1 ; i >= 0 ; i--){
+            System.out.println(numbers[i]);
+        }
+        }
+
+    }
+
