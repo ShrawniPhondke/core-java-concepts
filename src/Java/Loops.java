189e5bd5499b27add271cbfaec7c0eec68af15f0
Shrawni Phondke <shrawniphondke09@example.com>
2026-09-08 16:19:46 +0530

    Updated loops concept

--- src/Java/Loops.java
+++ src/Java/Loops.java
@@ -1,4 +1,18 @@
 package Java;
 
 public class Loops {
+    public static void main(String[]args){
+
+      for(int i =5; i<=50; i+=5){
+//          if(i % 2==0)
+          System.out.println(i);
+      }
+
+        String[] names = { "Sweta", "Gudly", "Amiya" };
+
+        for (String name : names) {
+            System.out.println("Name: " + name);
+        }
+
+    }
 }
